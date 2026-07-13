5010bc7967c04dea50237b24c9ecd76dea471e2c
vasavinagumalli24-sketch <vasavi.nagumalli24@sasi.ac.in>
2026-07-13 20:27:57 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const dns=require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 
 function connectDB() {
-
-    mongoose.connect("mongodb+srv://vasavinagumalli24_db_user:(vasavi@123)@cluster0.twzyu3h.mongodb.net/vasavi?appname=cluster0");
+mongoose.connect("mongodb+srv://vasavinagumalli24_db_user:<db_password>@cluster0.twzyu3h.mongodb.net/vasavi?appname=cluster0");
 
     mongoose.connection.on("connected", function () {
         console.log("MongoDB Connected");
